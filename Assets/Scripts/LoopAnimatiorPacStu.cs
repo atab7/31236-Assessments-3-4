2a0b0f2d6ec3ef33e50348d1b2c3973fa82764f3
Mehmet Ata Bal <13396700@student.uts.edu.au>
2022-09-25 23:48:06 +1000

    animation loop scripts added for assessment 3

--- Assets/Scripts/LoopAnimatiorPacStu.cs
+++ Assets/Scripts/LoopAnimatiorPacStu.cs
@@ -2,71 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoopAnimateGhost : MonoBehaviour
+public class LoopAnimatiorPacStu : MonoBehaviour
 {
-
     private Animator animator;
     private float timer;
-
 
     // Start is called before the first frame update
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
         timer = 0.0f;
-        animator.SetBool("isScared", false);
-        animator.SetBool("isRecovering", false);
+        animator.SetInteger("Direction", 0);
         animator.SetBool("isDead", false);
-        animator.SetInteger("Direction", 3);
-
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= 7)
+        if (timer >= 6)
         {
-            animator.SetBool("isScared", false);
-            animator.SetBool("isRecovering", false);
+            animator.SetInteger("Direction", 0);
             animator.SetBool("isDead", false);
-            animator.SetInteger("Direction", 3);
             timer = 0.0f;
 
-            
-        }
 
-        else if (timer >= 6)
-        {
-            animator.SetBool("isDead", true);
-            
         }
 
         else if (timer >= 5)
         {
-            animator.SetBool("isScared", false);
-            animator.SetBool("isRecovering", true);
+            animator.SetInteger("Direction", -1);
+            animator.SetBool("isDead", true);
+
         }
 
         else if (timer >= 4)
         {
-            animator.SetBool("isScared", true);
+
+            animator.SetInteger("Direction", 12);
         }
 
         else if (timer >= 3)
         {
-            animator.SetInteger("Direction", 12);
+            animator.SetInteger("Direction", 9);
+
         }
 
         else if (timer >= 2)
         {
-            animator.SetInteger("Direction", 9);
+            animator.SetInteger("Direction", 6);
         }
 
         else if (timer >= 1)
         {
-            animator.SetInteger("Direction", 6);
+            animator.SetInteger("Direction", 3);
         }
-
     }
 }
